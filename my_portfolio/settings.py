d0dc3f5483756dbf30165184cc238f4472825652
Daud <daud13t@gmail.com>
2025-12-01 10:43:19 +0000

    fixed allowed host

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-fallback-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'your-website.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'dauds-portfolio.onrender.com', 'localhost']
 
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
