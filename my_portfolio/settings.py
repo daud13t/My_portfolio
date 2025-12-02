da33d52fdc6ff7ab02c572d6713465e7c823a599
Daud <daud13t@gmail.com>
2025-12-02 10:33:44 +0000

    fixed csrf issue

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-fallback-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'dauds-portfolio.onrender.com', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'dauds-portfolio.onrender.com', '*.onrender.com', 'localhost']
 
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
