7c389b2cbb048ffa9ea9fd292713fe905cbbd76e
Daud <daud13t@gmail.com>
2026-01-06 21:40:39 +0500

    ignore db (broke)

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -86,12 +86,21 @@ WSGI_APPLICATION = 'my_portfolio.wsgi.application'
 import dj_database_url
 
 
-DATABASES = {
-    "default": dj_database_url.config(
-        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}"
-    )
-}
-
+if os.environ.get("DATABASE_URL"):
+    DATABASES = {
+        "default": dj_database_url.parse(
+            os.environ.get("DATABASE_URL"),
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
